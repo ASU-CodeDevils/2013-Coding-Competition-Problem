0185e9fe00e0976eb709d0d943cd129a00c011b6
Evan Borchert <eborchert6@gmail.com>
2017-05-27 12:12:22 -0400

    Problems Solved

--- src/main/java/com/sf/codingcomp/duck/DuckFinder.java
+++ src/main/java/com/sf/codingcomp/duck/DuckFinder.java
@@ -3,9 +3,16 @@ package com.sf.codingcomp.duck;
 public class DuckFinder {
 
 	public static <T> boolean isADuck(T objectToCheck) {
-
-		// TODO implement me
-		return false;
+		
+		try {
+		  objectToCheck.getClass().getMethod("looksLikeADuck");
+		  objectToCheck.getClass().getMethod("quacksLikeADuck");
+		  
+		  
+		} catch (NoSuchMethodException | SecurityException e) {
+		  return false;
+		}
+		return true;
 	}
 
 }
