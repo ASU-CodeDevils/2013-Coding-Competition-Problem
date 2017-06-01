04ed2650414f62541b2ea20bcfeb004c5dfc9511
Unknown <shawn_hulce@hotmail.com>
2017-05-31 21:25:14 -0500

    Working on Subnet
    Constructor isn't being called properly by test somehow. Inserting print statement into constructor as first line isn't reached. Debugging it steps over. Unsure where the problem is

--- src/main/java/com/sf/codingcomp/subnetutilities/SubnetImpl.java
+++ src/main/java/com/sf/codingcomp/subnetutilities/SubnetImpl.java
@@ -1,27 +1,109 @@
 package com.sf.codingcomp.subnetutilities;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 public class SubnetImpl implements Subnet {
-	public SubnetImpl(InetAddress subnet, int mask) throws InvalidMaskException {
+	
+	private InetAddress networkAddress;
+	private int netMask;
+	private InetAddress broadcastAddress;
+	
+	public SubnetImpl(InetAddress subnet, int mask) throws InvalidMaskException, UnknownHostException {
 		// TODO Auto-generated method stub
+		System.out.println("Made it!");
+		if ((mask > 32) || (mask <= 0)) { 
+			throw new InvalidMaskException();
+		}
+		this.networkAddress = calcNetworkAddress(subnet, mask);
+		this.broadcastAddress = calcBroadcastAddress(networkAddress, mask);
+		this.netMask = mask;
+		
 
 	}
 	
 	public boolean isIPInRange(InetAddress ipAddress) {
 		// TODO Auto-generated method stub
-		return false;
+		boolean result = true;
+		
+		return result;
+	}
+	
+	private InetAddress calcNetworkAddress(InetAddress subnet, int mask) throws UnknownHostException { 
+		int finalOctet = (mask / 8) + 1;
+		int finalOctetBits = mask % 8;
+		int finalOctetValue = 0;
+		int exponent = 7;
+		
+		for (int i = finalOctetBits; i > 0; i--) { 
+			finalOctetValue+= (int)Math.pow(2, exponent);
+			exponent--;
+		}
+		int numAddressPerSubnet = 256 - finalOctetValue;
+		int netAddressOctet = 0;
+		
+		while (subnet.getAddress()[finalOctet] > netAddressOctet ) { 
+			netAddressOctet+=numAddressPerSubnet;
+		}
+		netAddressOctet-= numAddressPerSubnet;
+		
+		byte[] netAddress = new byte[4];
+		
+		for (int i = 1; i < finalOctet; i++) { 
+			netAddress[i-1] = subnet.getAddress()[i-1];
+		}
+		netAddress[finalOctet] = (byte) netAddressOctet;
+		
+		for (int i = 4; i > finalOctet; i--) { 
+			netAddress[i - 1] = 0;
+		}
+		
+		InetAddress networkAddress = InetAddress.getByAddress(netAddress);
+		
+		return networkAddress;
 	}
 
 	public InetAddress getNetworkAddress() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		return networkAddress;
+	}
+	
+	private InetAddress calcBroadcastAddress(InetAddress netAddress, int mask) throws UnknownHostException { 
+		int finalOctet = (mask / 8) + 1;
+		int finalOctetBits = mask % 8;
+		int finalOctetValue = 0;
+		int exponent = 7;
+		for (int i = finalOctetBits; i > 0; i--) { 
+			finalOctetValue+= (int)Math.pow(2, exponent);
+			exponent--;
+		}
+		int numAddressPerSubnet = 256 - finalOctetValue;
+		int broadAddressOctet = 0;
+		
+		broadAddressOctet = netAddress.getAddress()[finalOctet] + numAddressPerSubnet - 1;
+		
+		byte[] broadAddress = new byte[4];
+		
+		for (int i = 1; i < finalOctet; i++) { 
+			broadAddress[i - 1] = netAddress.getAddress()[i - 1 ];
+		}
+		
+		broadAddress[finalOctet] = (byte) broadAddressOctet;
+		
+		for (int i = 4; i > finalOctet; i--) { 
+			broadAddress[i - 1] = (byte) 255;
+		}
+		
+		InetAddress broadcastAddress = InetAddress.getByAddress(broadAddress);
+		
+		return broadcastAddress;
 	}
 
 	public InetAddress getBroadcastAddress() {
 		// TODO Auto-generated method stub
-		return null;
+		return broadcastAddress;
 	}
 
 	public List<InetAddress> getAllAddresses() {
